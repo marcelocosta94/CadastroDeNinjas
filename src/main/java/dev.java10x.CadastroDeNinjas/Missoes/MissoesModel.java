4911e9e56d88d31fd98eaa0f6ad2b794f181f13d
Marcelo <marcelo.cas94@gmail.com>
2025-10-19 14:45:03 -0300

    Modelagem do banco de dados, NinjaModel e MissoesModel concluidos, anotations feitas para relacionamento de tabelas

---
+++ src/main/java/dev.java10x.CadastroDeNinjas/Missoes/MissoesModel.java
@@ -0,0 +1,23 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "missoes")
+public class MissoesModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String dificuldade;
+
+    @OneToMany(mappedBy = "missoes")
+    private List<NinjaModel> ninjas;
+
+}
--- src/main/java/dev.java10x.CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev.java10x.CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,4 +1,5 @@
-package dev.java10x.CadastroDeNinjas;
+package dev.java10x.CadastroDeNinjas.Ninjas;
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
 
 // Entity ele transforma uma classe em uma entidade do BD.
@@ -9,9 +10,16 @@ public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
     private int idade;
+
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") // Foreign Key
+    private MissoesModel missoes;
 
     public NinjaModel() {
     }
